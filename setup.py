eff1bef4646e37f7d90af12770b43e52464616f5
nift4 <36458596+nift4@users.noreply.github.com>
2019-02-09 14:27:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+name='pitft_touchscreen',
+version='1.0',
+description='Raspberry Pi screens.',
+author='PiGameDrv',
+url='https://pigamedrv.github.io',
+py_modules=['pitft_touchscreen']
+)
